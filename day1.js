368a64107c0784c066108e8c15e88d7769802204
Armoni Tigner <atigner2@student.gsu.edu>
2022-01-19 12:51:21 -0500

    initial commit

---
+++ day1.js
@@ -0,0 +1,43 @@
+ document.querySelector( "#changeColor").onclick = function(){
+const changeColor = document.querySelector("#container")
+changeColor.style.backgroundColor = "green"
+ }
+ document.querySelector( "#changeColor1").onclick = function(){
+    const changeColor = document.querySelector("#container")
+    changeColor.style.backgroundColor = "orange"
+     }
+    
+document.querySelector( "#changeColor2").onclick = function(){
+    const changeColor = document.querySelector("#container")
+    changeColor.style.backgroundColor = "yellow"
+     }
+     document.querySelector( "#changeColor3").onclick = function(){
+        const changeColor = document.querySelector("#container")
+        changeColor.style.backgroundColor = "Red"
+        console.log(changeColor.style)
+         }
+         document.querySelector( "#changeColor6").onclick = function(){
+            const changeColor = document.querySelector("#container")
+            changeColor.style.backgroundColor = "gold"
+            console.log(changeColor.style)
+             }
+             document.querySelector( "#changeColor7").onclick = function(){
+                const changeColor = document.querySelector("#container")
+                changeColor.style.backgroundColor = "blue"
+                console.log(changeColor.style)
+                 }
+        
+         document.querySelector( "#changeColor4").onclick = function(){
+            const changeColor = document.querySelector("#container")
+            changeColor.style.width = "200px"
+            changeColor.style.height = "200px"
+             }
+             document.querySelector( "#changeColor5").onclick = function(){
+                const changeColor = document.querySelector("#container")
+                changeColor.style.width = "400px"
+                changeColor.style.height = "400px"
+                 }
+                 
+                
+            
+            
